52d7b516e21105616be967c1fc6217a528d0e2e7
Billi Ramirez <ramirezbilly42@outlook.com>
2018-12-20 15:47:15 -0600

    compiling the file

--- block-scope.js
+++ block-scope.js
@@ -19,3 +19,12 @@ console.log(variable);
 console.log('Arrow Functions');
 var multiplyNumbers = function (nu1, nu2) { return nu1 * nu2; };
 console.log(multiplyNumbers(3, 2));
+//default parameters
+var coundown = function (start) {
+    if (start === void 0) { start = 24; }
+    console.log(start);
+    while (start > 0) {
+        start--;
+    }
+    console.log(start);
+};
